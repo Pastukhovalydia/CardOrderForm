87795f375bc0532d94a70472353d6f6bc97e26cd
Pastukhovalydia <pastuhovna@gmail.com>
2024-01-29 20:45:54 +0300

    Fix:test

--- src/test/java/ru/netology/CardOrderFormTest.java
+++ src/test/java/ru/netology/CardOrderFormTest.java
@@ -1,6 +1,7 @@
 package ru.netology;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CardOrderFormTest {
 
@@ -27,15 +28,23 @@ public class CardOrderFormTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+
+        // Открытие страницы в браузере вынесено в метод setup в качестве предусловия
+        driver.get("http://localhost:9999/");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
     public void testFormSubmissionSuccess() {
-        driver.get("http://localhost:9999/");
-
         // Ввод корректных данных
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Пастухова");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79139211093");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+71112225544");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
 
@@ -46,50 +55,58 @@ public class CardOrderFormTest {
 
     @Test
     public void testFormSubmissionInvalidName() {
-        driver.get("http://localhost:9999/");
-
         // Ввод некорректных данных (имя содержит цифры)
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Пастухова1 Лидия");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79139211093");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+71112225566");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
 
-        // Проверка отсутствия успешного сообщения
-        boolean successMessagePresent = driver.findElements(By.cssSelector("[data-test-id=order-success]")).size() > 0;
-        assertEquals(false, successMessagePresent);
+        // Проверка наличия сообщения об ошибке
+        String errorMessage = driver.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", errorMessage.trim());
     }
 
     @Test
     public void testFormSubmissionInvalidPhone() {
-        driver.get("http://localhost:9999/");
-
         // Ввод некорректных данных (неверный формат номера телефона)
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Пастухова Лидия");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79134452255");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("1122");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
 
-        // Проверка отсутствия успешного сообщения
-        boolean successMessagePresent = driver.findElements(By.cssSelector("[data-test-id=order-success]")).size() > 0;
-        assertEquals(false, successMessagePresent);
+        // Проверка наличия сообщения об ошибке
+        String errorMessage = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", errorMessage.trim());
     }
+
 
     @Test
     public void testFormSubmissionUncheckedCheckbox() {
-        driver.get("http://localhost:9999/");
-
         // Ввод корректных данных, но без установленного чекбокса
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Пастухова Лидия");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79139211093");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+71112225566");
         // Не устанавливаем чекбокс
         driver.findElement(By.className("button")).click();
 
-        // Проверка отсутствия успешного сообщения
-        boolean successMessagePresent = driver.findElements(By.cssSelector("[data-test-id=order-success]")).size() > 0;
-        assertEquals(false, successMessagePresent);
+        // Проверка наличия сообщения об ошибке
+        String errorMessage = driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__text")).getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", errorMessage.trim());
     }
 
+    @Test
+    public void testFormSubmissionEmptyFields() {
+        // Ввод пустых данных
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("");
+        driver.findElement(By.className("button")).click();
 
+        // Проверка наличия сообщений об ошибке для каждого поля
+        String nameErrorMessage = driver.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", nameErrorMessage.trim());
+
+        String phoneErrorMessage = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", phoneErrorMessage.trim());
+    }
 }
 
 
